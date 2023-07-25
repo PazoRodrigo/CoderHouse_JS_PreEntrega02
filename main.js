5ed08bbbe4a52a6273a751398d248ef7f8937d98
Rodrigo Pazo <pazo.rodrigo@gmail.com>
2023-07-25 16:32:11 -0300

    Se agrega botonera con algunas funcionalidades. Pop / Push

--- main.js
+++ main.js
@@ -1,25 +1,176 @@
-function fnMap(){}
-function fnFilter(){}
-function fnForEach(){}
-function fnFind(){}
-function fnSort(){}
-function fnSome(){}
-function fnEvery(){}
-function fnConcat(){}
-function fnIncludes(){}
-function fnJoin(){}
-function fnReduce(){}
-function fnIndexOf(){}
-function fnFindeIndex(){}
-function fnFill(){}
-function fnPush(){}
-function fnPop(){}
-function fnShift(){}
-function fnUnshift(){}
-function fnSlice(){}
-function fnReverse(){}
-function fnSplice(){}
-function fnLastIndexOf(){}
-function fnFlat(){}
-function fnIsArray(){}
-function fnFrom(){}
+function fnMap() {
+  LimpiarConsola();
+  mostrarConsolaMap();
+}
+function fnFilter() {
+  LimpiarConsola();
+  mostrarConsolaFilter();
+}
+function fnForEach() {
+  LimpiarConsola();
+  mostrarConsolaForEach();
+}
+function fnFind() {
+  LimpiarConsola();
+  mostrarConsolaFind();
+}
+function fnSort() {
+  LimpiarConsola();
+  mostrarConsolaSort();
+}
+function fnSome() {
+  LimpiarConsola();
+  mostrarConsolaSome();
+}
+function fnEvery() {
+  LimpiarConsola();
+  mostrarConsolaEvery();
+}
+function fnConcat() {
+  LimpiarConsola();
+  mostrarConsolaConcat();
+}
+function fnIncludes() {
+  LimpiarConsola();
+  mostrarConsolaIncludes();
+}
+function fnJoin() {
+  let vector = [];
+  let mensaje =
+    "Join. Llenará el arreglo para luego agregarle que una los elementos";
+  llenarVector(vector, mensaje);
+  do {
+    obj = prompt(
+      "longitud del Arreglo: " + vector.length + "Ingrese '--' para terminar"
+    );
+    if (obj !== "--") {
+      console.log(vector.join(obj));
+    }
+  } while (obj != "--");
+}
+function fnReduce() {
+  LimpiarConsola();
+  mostrarConsolaReduce();
+}
+function fnIndexOf() {
+  LimpiarConsola();
+  mostrarConsolaIndexOf();
+}
+function fnFindIndexOf() {
+  LimpiarConsola();
+  mostrarConsolaFindIndexOf();
+}
+function fnFill() {
+  LimpiarConsola();
+  mostrarConsolaFill();
+}
+function fnPush() {
+  LimpiarConsola();
+  mostrarConsolaPush();
+  let vector = [];
+  let mensaje = "Push. Llenará el arreglo";
+  llenarVector(vector, mensaje);
+}
+function fnPop() {
+  LimpiarConsola();
+  mostrarConsolaPop();
+  let vector = [];
+  let mensaje = "Pop. Llenará el arreglo para luego quitarle elementos";
+  llenarVector(vector, mensaje);
+  do {
+    obj = prompt(
+      "longitud del Arreglo: " +
+        vector.length +
+        " Ingrese 'xx' para borrar elemento. '--' para terminar"
+    );
+    if (obj === "xx") {
+      vector.pop();
+      mostrarVectorResultante(vector);
+    }
+  } while (obj != "--" || vector.length == 0);
+}
+function fnShift() {
+  LimpiarConsola();
+  mostrarConsolaShift();
+}
+function fnUnshift() {
+  LimpiarConsola();
+  mostrarConsolaUnshift();
+}
+function fnSlice() {
+  LimpiarConsola();
+  mostrarConsolaSplice();
+}
+function fnReverse() {
+  LimpiarConsola();
+  mostrarConsolaReverse();
+}
+function fnSplice() {
+  LimpiarConsola();
+  mostrarConsolaSlice();
+}
+function fnLastIndexOf() {
+  LimpiarConsola();
+  mostrarConsolaLastIndexOf();
+}
+function fnFlat() {
+  LimpiarConsola();
+  mostrarConsolaFlat();
+}
+function fnIsArray() {
+  LimpiarConsola();
+  mostrarConsolaIsArray();
+}
+function fnFrom() {
+  LimpiarConsola();
+  mostrarConsolaFrom();
+}
+
+const mostrarVectorResultante = (vectorVisualizar) =>
+  console.table(vectorVisualizar);
+const LimpiarConsola = () => console.clear();
+
+function llenarVector(vectorUtilizar, mensaje) {
+  alert(mensaje);
+  let obj;
+  do {
+    obj = prompt(
+      "longitud del Arreglo: " +
+        vectorUtilizar.length +
+        " Ingrese el nuevo elemento. '--' para terminar"
+    );
+    if (obj !== "--") {
+      vectorUtilizar.push(obj);
+      mostrarVectorResultante(vectorUtilizar);
+    }
+  } while (obj != "--");
+  return vectorUtilizar;
+}
+function TodosEnConsola() {
+  LimpiarConsola();
+  mostrarConsolaMap();
+  mostrarConsolaFilter();
+  mostrarConsolaForEach();
+  mostrarConsolaFind();
+  mostrarConsolaSort();
+  mostrarConsolaSome();
+  mostrarConsolaEvery();
+  mostrarConsolaConcat();
+  mostrarConsolaIncludes();
+  mostrarConsolaJoin();
+  mostrarConsolaReduce();
+  mostrarConsolaIndexOf();
+  mostrarConsolaFindIndexOf();
+  mostrarConsolaFill();
+  mostrarConsolaPush();
+  mostrarConsolaPop();
+  mostrarConsolaShift();
+  mostrarConsolaUnshift();
+  mostrarConsolaSplice();
+  mostrarConsolaReverse();
+  mostrarConsolaSlice();
+  mostrarConsolaLastIndexOf();
+  mostrarConsolaFlat();
+  mostrarConsolaIsArray();
+  mostrarConsolaFrom();
+}
